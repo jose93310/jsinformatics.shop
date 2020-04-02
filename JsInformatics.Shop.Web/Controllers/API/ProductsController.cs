e2f61e2addb3f493cc40e25066a188c424b522b8
jose93310 <62722135+jose93310@users.noreply.github.com>
2020-04-02 09:39:52 -0400

    Add ApiProductsController

---
+++ JsInformatics.Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿
+namespace JsInformatics.Shop.Web.Controllers.API
+{
+    using JsInformatics.Shop.Web.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
