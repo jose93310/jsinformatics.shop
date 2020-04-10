d39e4f7310b4a0168a3e61f752ae999e6e7d6ca9
jose93310 <62722135+jose93310@users.noreply.github.com>
2020-04-10 19:19:20 -0400

    Add images in products

---
+++ JsInformatics.Shop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace JsInformatics.Shop.Web.Models
+{
+    using JsInformatics.Shop.Web.Data.Entities;
+    using Microsoft.AspNetCore.Http;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProductViewModel : Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set;  }
+    }
+}
